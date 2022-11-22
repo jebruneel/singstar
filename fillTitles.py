6cf36a2570f2534a7b25babb16ad6fd743908527
Jeroen Bruneel <jebruneel@gmail.com>
2022-11-22 22:25:32 +0100

    new links

--- fillTitles.py
+++ fillTitles.py
@@ -1,7 +1,5 @@
 import os
 
-
-
 dir = "C:/projects/singstar/txtfiles/"
 inputfile = "c:/projects/singstar/inputfile.txt"
 os.remove(inputfile)
@@ -11,4 +9,4 @@ fout = open(inputfile,"wt",encoding="utf-8")
 for title in os.listdir(dir):
     fout.write(title + '\n')
 
-fout.close()
+fout.close()    
