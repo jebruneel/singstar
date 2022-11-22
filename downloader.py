abf6659b0b808f8ab6ecad08d96d68291b5a6440
Jeroen Bruneel <jeroen.bruneel@esc.be>
2022-11-22 16:59:51 +0100

    with conversion

--- downloader.py
+++ downloader.py
@@ -2,16 +2,41 @@ from yt_dlp import YoutubeDL
 import os
 import shutil
 
-DownloadedTxtFilesLocation = "c:/me/projects/singstardownloader/txtfiles/"
-combinedFile = "C:/me/projects/singstardownloader/combinedlinksandtitles.txt"
+DownloadedTxtFilesLocation = "c:/me/projects/singstar/txtfiles/"
+combinedFile = "C:/me/projects/singstar/combinedlinksandtitles.txt"
+
+
+def changetxtfile(title,mp3value,mp4value):
+    print('lol')
+
 
 def downloadLink(title, videolink):
     songlocation = "c:/me/test/" + title
     os.mkdir(songlocation)
     shutil.copy(DownloadedTxtFilesLocation + title + ".txt", songlocation  + "/" + title + ".txt")
-    ydl_opts = {'outtmpl': songlocation + "/" + title + ".mp3"}
+    os.chdir(songlocation)
+    #download the whole video
+    ydl_opts = {'outtmpl': songlocation + "/" + title + '.%(ext)s'}
     with YoutubeDL(ydl_opts) as ydl:
+        # info = ydl.extract_info(videolink, download=False)
+        # file_extensions = ydl.prepare_filename(info)
+        # print('banana ' + file_extensions)
         ydl.download(videolink)
+    #download as mp3
+    # ydl_opts = {'format': 'bestaudio/best',
+    #             'postprocessors': [{
+    #                 'key': 'FFmpegExtractAudio',
+    #                 'preferredcodec': 'mp3',
+    #                 'preferredquality': '320',
+    # }]}
+    # with YoutubeDL(ydl_opts) as ydl:
+    #     ydl.download(videolink)
+
+    #change text file to include video and audio
+    mp3value = title + '.mp3'
+    mp4value = title + '.mp4'
+
+    changetxtfile(title,mp3value,mp4value)
 
 video = ""
 title = ""
