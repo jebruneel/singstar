adc24677f9476111174f839395fd7dd7e186e9f6
Jeroen Bruneel <jebruneel@gmail.com>
2022-11-23 16:42:57 +0100

    combine inout

--- downloader.py
+++ downloader.py
@@ -2,6 +2,9 @@ from yt_dlp import YoutubeDL
 import os
 import shutil
 import time
+import os
+import urllib.request
+import re
 
 # DownloadedTxtFilesLocation = "c:/me/projects/singstar/txtfiles/"
 # inputfile = "C:/me/projects/singstar/combinedlinksandtitles.txt"
@@ -56,10 +59,42 @@ def downloadLink(title, videolink):
 
     changetxtfile(title,mp3value,mp4value)
 
+
+
+
+def findvideo(search_keyword):
+    words = search_keyword.split()
+
+    url = "http://www.youtube.com/results?search_query="
+
+    for word in words:
+        url += word + "+"
+
+    html = urllib.request.urlopen(url[:-1])
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    fout.write("https://www.youtube.com/watch?v=" + video_ids[0] + '\n')
+
+#get all textfiles in folder
+dir = "C:/projects/singstar/txtfiles/"
+os.remove(inputfile)
+
+fout = open(inputfile,"wt",encoding="utf-8")
+#put them in the inputfile.txt with the first found youtube vid
+for title in os.listdir(dir):
+    songtitle = title[0:len(title) - 4]
+    fout.write(songtitle + '\n')
+    findvideo(songtitle)
+
+fout.close()    
+
 video = ""
 title = ""
 index = 1
 
+#read the inputfile title and
+#create folder
+#download + convert video
+#change txt file to include downloaded stuff
 d = {}
 with open(inputfile) as f:
     for line in f:
